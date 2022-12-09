51ba4307cba9b68435be3eaa0ab99f1d4f162dfb
SrGesus <108523575+SrGesus@users.noreply.github.com>
2022-12-09 07:23:46 +0000

    Using rust binaries instead of cargo

--- get_cmd.sh
+++ get_cmd.sh
@@ -4,17 +4,23 @@ ROOT_DIR=$PWD
 
 case $1 in
 abread)
-    echo cd $PWD/abread "&&" cat $PWD/inputs/$2.txt "|" cargo run --release --bin day$2
+    cd $PWD/abread && cargo build --release --bin day$2
+    cd $ROOT_DIR
+    echo cd $PWD/abread "&&" cat $PWD/inputs/$2.txt "|" ./target/release/day$2
 ;;
 borges)
     rm $PWD/borges/2022/$(printf 'day%02d' $2)/input.txt
     ln -s $PWD/inputs/$2.txt $PWD/borges/2022/$(printf 'day%02d' $2)/input.txt
-    echo cd $PWD/borges/2022/$(printf 'day%02d' $2) "&&" cargo run --release --bin part2
+    cd $PWD/borges/2022/$(printf 'day%02d' $2) && cargo build --release --bin part2
+    cd $ROOT_DIR
+    echo cd $PWD/borges/2022/$(printf 'day%02d' $2) "&&" ./target/release/part2
 ;;
 correia)
     rm $PWD/correia/2022/inputs/$(printf 'day%02d.txt' $2)
     ln -s $PWD/inputs/$2.txt $PWD/correia/2022/inputs/$(printf 'day%02d.txt' $2)
-    echo cd $PWD/correia/2022/ "&&" cargo run --release $2
+    cd $PWD/correia/2022 && cargo build --release
+    cd $ROOT_DIR
+    echo cd $PWD/correia/2022/ "&&" ./target/release/aoc2022 $2
 ;;
 dacops)
     cd dacops/AoC_22/Day_$2
@@ -28,19 +34,25 @@ espadeiro)
         echo cd $PWD/espadeiro/aoc2022/$(printf 'day%02d' $2) "&&" cargo run --release $PWD/inputs/$2.txt
         ;;
         *)
-        echo cd $PWD/espadeiro/aoc2022/$(printf 'day%02d' $2) "&&" cat $PWD/inputs/$2.txt "|" cargo run --release
+        cd $PWD/espadeiro/aoc2022/$(printf 'day%02d' $2) && cargo build --release 
+        cd $ROOT_DIR
+        echo cd $PWD/espadeiro/aoc2022/$(printf 'day%02d' $2) "&&" cat $PWD/inputs/$2.txt "|" ./target/release/$(printf 'day%02d' $2)
         ;;
     esac
 ;;
 rso)
     rm $PWD/rso/2022/inputs/$(printf 'day%02d.txt' $2)
     ln -s $PWD/inputs/$2.txt $PWD/rso/2022/inputs/$(printf 'day%02d.txt' $2)
-    echo cd $PWD/rso/2022 "&&" cargo run --release $2
+    cd $PWD/rso/2022 && cargo build --release
+    cd $ROOT_DIR
+    echo cd $PWD/rso/2022 "&&" ./target/release/aoc2022 $2
 ;;
 srgesus)
     rm $PWD/srgesus/AoC2022/resources/$(printf 'day%02d' $2)/input.txt
     ln -s $PWD/inputs/$2.txt $PWD/srgesus/AoC2022/resources/$(printf 'day%02d' $2)/input.txt
-    echo cd $PWD/srgesus/AoC2022/ "&&" cargo run --release --bin $(printf 'day%02d-2' $2)
+    cd $PWD/srgesus/AoC2022/ && cargo build --release --bin $(printf 'day%02d-2' $2)
+    cd $ROOT_DIR
+    echo cd $PWD/srgesus/AoC2022/ "&&" ./target/release/$(printf 'day%02d-2' $2)
 ;;
 gaspar)
     case $2 in
